a1323807f9312181c2c0cac0ac69068c2d6222ea
PrShivashish <141547525+PrShivashish@users.noreply.github.com>
2025-11-12 21:03:46 +0530

    Update page.jsx

--- app/(public)/shop/page.jsx
+++ app/(public)/shop/page.jsx
@@ -1,11 +1,31 @@
-'use client'
 import { Suspense } from "react"
 import ProductCard from "@/components/ProductCard"
 import { MoveLeftIcon } from "lucide-react"
 import { useRouter, useSearchParams } from "next/navigation"
 import { useSelector } from "react-redux"
 
- function ShopContent() {
+export const metadata = {
+  title: 'Shop All Products | GoCart',
+  description:
+    'Browse thousands of products from multiple sellers. Find electronics, fashion, and more at competitive prices.',
+  keywords: ['shop', 'products', 'buy online', 'marketplace'],
+  
+  openGraph: {
+    title: 'Shop All Products | GoCart',
+    description: 'Browse thousands of products from sellers.',
+    url: 'https://gocart-ecommerce-ruddy.vercel.app/shop',
+    type: 'website',
+    images: [
+      {
+        url: 'https://gocart-ecommerce-ruddy.vercel.app/og-image.jpg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+};
+
+function ShopContent() {
 
     // get query params ?search=abc
     const searchParams = useSearchParams()
@@ -31,7 +51,6 @@ import { useSelector } from "react-redux"
         </div>
     )
 }
-
 
 export default function Shop() {
   return (
